45c73df2adc4b9a26f0cbcf38e19ef5257d1686a
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-10-23 17:12:57 -0300

    Default content deleted or commented, testing Header, Empleos and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from "react";
+// import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import IndexMain from "./components/IndexMain";
+import Empleos from "./components/Empleos";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <Header />
+      {/* <main >
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        
+      </main> */}
+
+      {/* <IndexMain /> */}
+      <Empleos/>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
