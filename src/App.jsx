962aefde51d7be83a4e0e71620e97712851b5d54
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-10-30 20:46:19 -0300

    SearchFormSection component added, data.json is now imported in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import IndexMain from "./components/IndexMain";
 import JobList from "./components/JobList";
 import Pagination from "./components/Pagination";
+import SearchFormSection from "./components/SearchFormSection";
+import jobsData from './data.json'
 
 function App() {
-  // const [count, setCount] = useState(0);
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const RESULTS_PER_PAGE = 5
+  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE)
+  const pagedResults = jobsData.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE,
+    currentPage * RESULTS_PER_PAGE
+  )
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page)
+  }
 
   return (
     <>
       <Header/>
-      {/* <main >
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </main> */}
 
-      {/* <IndexMain /> */}
-      <JobList/>
-      <Pagination/>
+      <main>
+
+        <SearchFormSection/>
+
+        <section>
+          <JobList jobs={pagedResults} />
+          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+        </section>
+      </main>
+
       <Footer/>
     </>
   );
