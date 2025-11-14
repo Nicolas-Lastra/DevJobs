bb33568e643c72a247639beca2a6f5ab7962ae8e
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-14 17:03:54 -0300

    Not found page added as a default page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SearchPage from "./pages/Search";
 import HomePage from "./pages/Home";
+import { NotFoundPage } from "./pages/404";
 
 function App() {
   const currentPath = window.location.pathname
 
+  let page = <NotFoundPage/>
+  if (currentPath === '/') {
+    page = <HomePage/>
+  } else if (currentPath === '/search') {
+    page = <SearchPage/>
+  }
+
   return (
     <>
       <Header />
-      {
-        currentPath === '/' ? <HomePage/> : <SearchPage/>
-      }
+      {page}
       <Footer />
     </>
   );
