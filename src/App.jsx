0e4b90f2fef27e9436c3ee010b46ef66494faf38
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-14 16:59:23 -0300

    The search page has been separated into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,17 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import JobList from "./components/JobList";
-import Pagination from "./components/Pagination";
-import SearchFormSection from "./components/SearchFormSection";
-import jobsData from './data.json'
+import SearchPage from "./pages/Search";
+import HomePage from "./pages/Home";
 
 function App() {
-
-  const [filters, setFilters] = useState({
-    search: '',
-    technology: '',
-    location: '',
-    experienceLevel: ''
-  })
-  const [textToFilter, setTextToFilter] = useState('')
-  const [currentPage, setCurrentPage] = useState(1)
-  const RESULTS_PER_PAGE = 5
-
-  const jobsFilteredByFilters = jobsData.filter(job => {
-    return (
-      (filters.technology === '' || job.data.technology === filters.technology) && 
-      (filters.location === '' || job.data.modalidad === filters.location) && 
-      (filters.experienceLevel === '' || job.data.nivel === filters.experienceLevel)
-    )
-  })
-
-  const jobsWithTextFilter = textToFilter === ''
-    ? jobsFilteredByFilters
-    : jobsFilteredByFilters.filter(job => {
-      return job.titulo.toLowerCase().includes(textToFilter.toLowerCase())
-    })
-
-  const totalPages = Math.max(1, Math.ceil(jobsWithTextFilter.length / RESULTS_PER_PAGE))
-
-  const pagedResults = jobsWithTextFilter.slice(
-    (currentPage - 1) * RESULTS_PER_PAGE,
-    currentPage * RESULTS_PER_PAGE
-  )
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page)
-  }
-
-  const handleSearch = (filters) => {
-    setFilters({
-      technology: filters.technology,
-      location: filters.location,
-      experienceLevel: filters.experienceLevel
-    })
-    setTextToFilter(filters.search || '')
-    setCurrentPage(1)
-  }
-
-  const handleTextFilter = (text) => {
-    setTextToFilter(text)
-    setCurrentPage(1)
-  }
+  const currentPath = window.location.pathname
 
   return (
     <>
       <Header />
-
-      <main>
-
-        <SearchFormSection onSearch={handleSearch} onTextFilter={handleTextFilter} />
-
-        <section>
-          <JobList jobs={pagedResults} />
-          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
-        </section>
-      </main>
-
+      {
+        currentPath === '/' ? <HomePage/> : <SearchPage/>
+      }
       <Footer />
     </>
   );
