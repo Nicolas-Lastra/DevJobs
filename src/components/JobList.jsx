83c2fabf7509a7b2d839980b22a7a02e5bc2ec8e
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-18 10:53:26 -0300

    Results title (h2) has been moved to the Search page. The document format has been updated.

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,22 +1,25 @@
 import JobCard from "./JobCard";
 
 export default function JobList({ jobs }) {
-
-    return (
-            <>
-                <h2 id="search-results-title">Resultados de búsqueda</h2>
-
-                <div className="jobs-listings">
-                    {
-                        jobs.length === 0 && (
-                            <p style={{ textAlign: 'center', padding: '1rem', textWrap: 'balance' }}>No se han encontrado empleos que coincidan con los criterios de búsqueda.</p>
-                        )
-                    }
-                    {jobs.map((job) => (
-                        <JobCard key={job.id} data={job}/>
-                    ))}
-                </div>
-
-            </>
-    );
+  return (
+    <>
+      <div className="jobs-listings">
+        {jobs.length === 0 && (
+          <p
+            style={{
+              textAlign: "center",
+              padding: "1rem",
+              textWrap: "balance",
+            }}
+          >
+            No se han encontrado empleos que coincidan con los criterios de
+            búsqueda.
+          </p>
+        )}
+        {jobs.map((job) => (
+          <JobCard key={job.id} data={job} />
+        ))}
+      </div>
+    </>
+  )
 }
