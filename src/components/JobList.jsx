7fb805380d24d6140be1d60c8753e0c3ac00e604
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-17 22:11:14 -0300

    Unnecessary comments were removed. An alternative is added when no match is found with the filters.

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,21 +1,17 @@
-// import { useState, useEffect } from "react";
 import JobCard from "./JobCard";
 
 export default function JobList({ jobs }) {
-    // const [data, setData] = useState([]);
-
-    // useEffect(() => {
-    //     fetch("/data.json")
-    //         .then((response) => response.json())
-    //         .then((jsonData) => setData(jsonData))
-    //         .catch((error) => console.error("Error de fetch de data: ", error));
-    // });
 
     return (
             <>
                 <h2 id="search-results-title">Resultados de búsqueda</h2>
 
                 <div className="jobs-listings">
+                    {
+                        jobs.length === 0 && (
+                            <p style={{ textAlign: 'center', padding: '1rem', textWrap: 'balance' }}>No se han encontrado empleos que coincidan con los criterios de búsqueda.</p>
+                        )
+                    }
                     {jobs.map((job) => (
                         <JobCard key={job.id} data={job}/>
                     ))}
