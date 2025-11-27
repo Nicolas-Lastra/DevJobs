09898e80636d8eb6a096600ca1bee48a5babe540
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-27 10:43:40 -0300

    Component that contains the authentication logic, provides an AuthProvider and a custom hook useAuth that use context API to read the global login state.

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+import { use } from "react"
+
+export const AuthContext = createContext()
+
+export function AuthProvider({ children }) {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const login = () => {
+    setIsLoggedIn(true)
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false)
+  }
+  
+  const value = {
+    isLoggedIn,
+    login,
+    logout
+  }
+
+  return <AuthContext value={value}>
+    {children}
+  </AuthContext>
+}
+
+export function useAuth() {
+    const context = use(AuthContext)
+
+    if (context === undefined) throw new Error('useAuth must be within an AuthProvider')
+    
+    return context
+}
