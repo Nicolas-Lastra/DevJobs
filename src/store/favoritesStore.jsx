f81cc6cf42aa17310915b86f0ca956d755b11398
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-27 16:40:25 -0300

    Context replaced by zustand. authStore manages login and favoritesStore manages a list of the user's favorite jobs.

---
+++ src/store/favoritesStore.jsx
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+
+export const useFavoritesStore = create((set, get) => ({
+    favorites: [],
+
+    addFavorite: (jobId) => {
+        set((state) => ({
+            favorites: state.favorites.includes(jobId)
+              ? state.favorites
+              : [...state.favorites, jobId]
+        }))
+    },
+
+    removeFavorite: (jobId) => {
+        set((state) => ({
+            favorites: state.favorites.filter((id) => id !== jobId)
+        }))
+    },
+
+    toggleFavorite: (jobId) => {
+        const { favorites, addFavorite, removeFavorite } = get()
+        const isFav = favorites.includes(jobId)
+        isFav ? removeFavorite(jobId) : addFavorite(jobId)
+    },
+
+    isFavorite: (jobId) => {
+        return get().favorites.includes(jobId)
+    },
+
+    countFavorites: () => get().favorites.length
+}))
