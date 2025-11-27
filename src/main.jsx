23ed4b0fe0ffa885367b1f077d452846f6429589
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-27 11:10:33 -0300

    App component wrapped in AuthProvider to access the authentication context in other components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router'
-import { StrictMode } from 'react'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 )
