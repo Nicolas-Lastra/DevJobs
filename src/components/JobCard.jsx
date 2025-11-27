dbd8abafeed7957b8e319c38f4cdb162b9a53852
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-27 16:49:23 -0300

    The data prop has been renamed job to avoid confusion. A button has been added to add or remove a job from the favorite's list.

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react"
 import Link from "./Link"
 import styles from "./JobCard.module.css"
+import { useFavoritesStore } from "../store/favoritesStore"
+import { useAuthStore } from "../store/authStore"
 
-export default function JobCard({ data }) {
+function JobCardFavoriteButton({ jobId }) {
+    const { toggleFavorite, isFavorite } = useFavoritesStore()
 
+    return (
+        <button onClick={() => toggleFavorite(jobId)}>
+            {isFavorite(jobId) ? '❤️' : '🤍'}
+        </button>
+    )
+}
+
+export default function JobCard({ job }) {
     const [isApplied, setIsApplied] = useState(false)
+    const { isLoggedIn } = useAuthStore()
 
     const handleApplyClick = () => {
         setIsApplied(true)
@@ -14,23 +26,24 @@ export default function JobCard({ data }) {
 
     return (
         <article
-            data-technology={data.data.technology}
-            data-modalidad={data.data.modalidad}
-            data-nivel={data.data.nivel}
+            data-technology={job.data.technology}
+            data-modalidad={job.data.modalidad}
+            data-nivel={job.data.nivel}
             className="job-listing-card"            
         >
             <div>
-                <Link className={styles.title} href={`/jobs/${data.id}`}>{data.titulo}</Link>
-                <small> {data.empresa} | {data.ubicacion}</small>
+                <Link className={styles.title} href={`/jobs/${job.id}`}>{job.titulo}</Link>
+                <small> {job.empresa} | {job.ubicacion}</small>
                 <p>
-                    {data.descripcion}
+                    {job.descripcion}
                 </p>
             </div>
             <div className={styles.actions}>
-                <Link href={`/jobs/${data.id}`} className={styles.details}>
+                <Link href={`/jobs/${job.id}`} className={styles.details}>
                     Ver detalles
                 </Link>
                 <button onClick={ handleApplyClick } disabled={isApplied} className={buttonClasses}>{buttonText}</button>
+                {isLoggedIn && <JobCardFavoriteButton jobId={job.id}/>}
             </div>
         </article>
     )
