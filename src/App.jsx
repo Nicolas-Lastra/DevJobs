b0fd15e4646863e68a511f49b99bac4b8b30c4ef
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-11-28 16:16:52 -0300

    Profile, login and register mock pages added. Profile page is enclosed by ProtectedRoute component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 import { Routes, Route } from 'react-router'
 
 const HomePage = lazy(() => import('./pages/Home.jsx'))
@@ -8,6 +9,9 @@ const SearchPage = lazy(() => import('./pages/Search.jsx'))
 const Contact = lazy(() => import('./pages/Contact'))
 const JobDetail = lazy(() => import('./pages/Detail'))
 const NotFoundPage = lazy(() => import('./pages/404'))
+const ProfilePage = lazy(() => import('./pages/Profile.jsx'))
+const Login = lazy(() => import('./pages/Login.jsx'))
+const Register = lazy(() => import('./pages/Register.jsx'))
 
 function App() {
 
@@ -21,7 +25,14 @@ function App() {
           <Route path="/search" element={<SearchPage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/jobs/:jobId" element={<JobDetail />} />
+          <Route path="/profile" element={
+            <ProtectedRoute redirectTo="/login">
+              <ProfilePage />
+            </ProtectedRoute>
+          } />
           <Route path="*" element={<NotFoundPage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
       </Suspense>
       <Footer />
